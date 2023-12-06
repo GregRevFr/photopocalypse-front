bd0b22ee0a71742e141ecca0ab356f20b901ba06
Leon Pawelzik <leon.pawelzik@gmail.com>
2023-12-06 16:34:59 +0100

    Implemented API

--- main_app.py
+++ main_app.py
@@ -3,22 +3,63 @@ from streamlit_option_menu import option_menu
 import requests
 import json
 import base64
+from PIL import Image
+from io import BytesIO
 
-st.set_page_config(page_title="BLURBUSTER",
-                    page_icon=":bar_chart:",
-                    layout="wide")
+# Function to send file to server
+def send_file_to_server(file):
+    """
+    Sends a file to the server using a POST request.
 
-cards = []
+    Args:
+        file: The file to be sent.
 
+    Returns:
+        The response from the server.
+    """
+    url = 'https://phurge-api-ieuwqkua2q-ew.a.run.app/upload-image/'
+    files = {'file': (file.name, file, 'multipart/form-data')}
+    response = requests.post(url, files=files)
+    return response
+
+# Function to convert image to base64
+def image_to_base64(image):
+    """
+    Convert an image to base64 encoding.
+
+    Args:
+        image: The image to be converted.
+
+    Returns:
+        The base64 encoded string representation of the image.
+    """
+    buffered = BytesIO()
+    image.save(buffered, format="PNG")
+    return base64.b64encode(buffered.getvalue()).decode()
+
+# Function to build the main page
 def build_blurnotblur_page():
+    """
+    Build the blur-not-blur page.
 
+    This function creates a page where users can upload images and check if they are blurry or not.
+    It displays the uploaded images along with their blur percentage.
+
+    Returns:
+        None
+    """
+    # Set the page title
     st.markdown(
-    f'<h1 style="color: {"#000099"};">Check if is blur</h1>',
-    unsafe_allow_html=True
-)
-    uploaded_files = st.file_uploader("Choose a file",
-                                    accept_multiple_files=True,
-                                    type=["txt", "csv", "pdf", "json", "png", "jpg", "svg", "jpeg"])
+        f'<h1 style="color: {"#000099"};">Check if is blur</h1>',
+        unsafe_allow_html=True
+    )
+
+    # Allow users to upload files
+    uploaded_files = st.file_uploader(
+        "Choose a file",
+        accept_multiple_files=True,
+        type=["txt", "csv", "pdf", "json", "png", "jpg", "svg", "jpeg"]
+    )
 
     # Position the file uploader at the upper-left corner
     st.markdown(
@@ -28,69 +69,48 @@ def build_blurnotblur_page():
         unsafe_allow_html=True
     )
 
+
+    cards = [] # Initialize cards list
+
+    # Process each uploaded file
     for file in uploaded_files:
-        # Check if the file is an image
-        if 'image' in file.type:
+        response = send_file_to_server(file)
 
-            params = {
-                'models': 'properties',
-                'api_user': '1947745726',
-                'api_secret': 'CjnMaFVutPnmaQHMSjuKD8rgbG'
+        # if response.status_code == 200:
+            # st.success(f'File {file.name} successfully uploaded and processed by server.')
+
+        # Convert the response content to an image and then to base64
+        image = Image.open(BytesIO(response.content))
+        base64_image = image_to_base64(image)
+
+        # Create a card with image details
+        cards.append({
+            "title": "Image",
+            "text": "This image has a {image_sharpness}% of blur.",
+            "image": f"data:image/png;base64,{base64_image}",
+            "styles": {
+                'card': {
+                    "width": "250px",
+                    "height": "280px",
+                    "margin-right": "10px",
+                    "margin-bottom": "30px",
+                    "border-radius": "10px",
+                    "box-shadow": "0 0 10px rgba(0,0,0,0.5)",
+                    "display": "flex",
+                    "flex-direction": "column",
+                    "align-items": "center"
+                },
+                'text': {
+                    "font-family": "calibri"
+                }
+            },
+            "position": {
+                "top": "1000px",
+                "left": "100px"
             }
-            files = {'media': file}
-            r = requests.post('https://api.sightengine.com/1.0/check.json',
-                              files=files, data=params)
+        })
 
-            if r.status_code == 200:
-                # st.success('API Called With success...Updating Cards Parameters')
-                # Get the content of the uploaded file
-                file.seek(0)
-
-                file_content = file.read()
-
-                file_extension = file.type.split("/")[-1]
-
-                # Display the card with the uploaded image
-                encoded = base64.b64encode(file_content)
-                data = f"data:image/{file_extension};base64,{encoded.decode('utf-8')}"
-                output = json.loads(r.text)
-                image_sharpness = output.get('sharpness', 0)
-
-                cards.append({
-                    "title": "Image",
-                    "text": f"This image has a {image_sharpness}% of blur.",
-                    "image": data,
-                    "styles": {
-                        'card': {
-                            "width": "250px",
-                            "height": "280px",
-                            "margin-right": "10px",
-                            "margin-bottom": "30px",
-                            "border-radius": "10px",
-                            "box-shadow": "0 0 10px rgba(0,0,0,0.5)",
-                            "display": "flex",
-                            "flex-direction": "column",
-                            "align-items": "center"
-                        },
-                        'text': {
-                            "font-family": "calibri"
-                        }
-                    },
-                    "position": {
-                        "top": "1000px",
-                        "left": "100px"
-                    }
-                })
-                st.markdown(
-                '<style>'
-                'div[data-testid="cards.append"] { position: absolute; top: 1000px; left: 0px; }'
-                '</style>',
-                unsafe_allow_html=True
-                )
-
-            else:
-                st.warning('Something went wrong!')
-
+    # Display the cards
     display_columns(cards)
 
 def card(title, text, image, styles, position=None):
@@ -98,28 +118,38 @@ def card(title, text, image, styles, position=None):
     Generate HTML content for a card.
 
     Args:
-        title (str): The title of the card.
-        text (str): The text of the card.
-        image (str): URL or base64 encoded image for the card.
-        styles (dict): Dictionary containing styles for the card.
-        position (dict): Dictionary containing position properties (top, left) for the card.
+    title (str): The title of the card.
+    text (str): The text of the card.
+    image (str): URL or base64 encoded image for the card.
+    styles (dict): Dictionary containing styles for the card.
+    position (dict): Dictionary containing position properties (top, left) for the card.
 
     Returns:
-        str: HTML content of the card.
+    str: HTML content of the card.
     """
     card_style = "; ".join(f"{key}: {value}" for key, value in styles.get("card", {}).items())
     text_style = "; ".join(f"{key}: {value}" for key, value in styles.get("text", {}).items())
 
     html_content = f"""
-        <div style="{card_style}">
-            <h3 style="{text_style}">{title}</h3>
-            <p style="{text_style}">{text}</p>
-            <img src="{image}" alt="{title}" style="width: 70%; max-width: 70%; height: auto; margin-top: 20px;">
-        </div>
+    <div style="{card_style}">
+        <h3 style="{text_style}">{title}</h3>
+        <p style="{text_style}">{text}</p>
+        <img src="{image}" alt="{title}" style="width: 70%; max-width: 70%; height: auto; margin-top: 20px;">
+    </div>
     """
     return html_content
 
 def display_columns(cards):
+    """
+    Display the given cards in multiple columns.
+
+    Args:
+        cards (list): A list of dictionaries representing the cards to be displayed.
+            Each dictionary should have the following keys: "title", "text", "image", "styles".
+
+    Returns:
+        None
+    """
     num_columns = 3
     num_cards = len(cards)
 
@@ -144,18 +174,22 @@ def display_columns(cards):
                 card_index += 1
 
 def sidebar_menu():
+    """
+    Function to display a sidebar menu with options and icons.
+    The selected option determines which page to build.
+    """
     with st.sidebar:
-        selected = option_menu(menu_title="BLURBUSTER",
-                               options=["home","blurnotblur"],
-                               icons=["house","rocket"],
-                               menu_icon="cast",
-                               default_index=1,
-                               styles={
-                                    "icon": {"color": "#0080FF", "font-size": "25px"},
-                                    "nav-link-selected": {"background-color": "#000099"},
-                                    #"nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-                                    #"container": {"padding": "0!important", "background-color": "#fafafa", "font-family": "Permanent Marker"},
-                                })
+        selected = option_menu(
+            menu_title="BLURBUSTER",
+            options=["home","blurnotblur"],
+            icons=["house","rocket"],
+            menu_icon="cast",
+            default_index=1,
+            styles={
+                "icon": {"color": "#0080FF", "font-size": "25px"},
+                "nav-link-selected": {"background-color": "#000099"},
+            }
+        )
 
     if selected == "blurnotblur":
         build_blurnotblur_page()
@@ -163,23 +197,26 @@ def sidebar_menu():
         pass
 
 def image_logo():
+    """
+    Display the logo image on the webpage.
+
+    This function displays the logo image on the webpage using the provided image path.
+    It also checks if the image file exists and prints a message accordingly.
+
+    Returns:
+        None
+    """
     col1, col2, col3 = st.columns([1, 2, 3])
 
     # Leave the first and second column empty
     with col3:
         image_path = 'photos/Captura.png'
-        print(f"Image Path: {image_path}")
 
-        # Check if the file exists
-        import os
-        if os.path.exists(image_path):
-            print("Image file exists.")
-        else:
-            print("Image file does not exist. Please check the file path.")
 
         st.markdown(
             f'<img src="data:image/png;base64,{base64.b64encode(open(image_path, "rb").read()).decode()}" style="position: absolute; top: 0px; right: 0px; max-width: 25%;">',
-            unsafe_allow_html=True)
+            unsafe_allow_html=True
+        )
 
 image_logo()
 
