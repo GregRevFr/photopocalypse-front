b4ad210aa032d53327a742b8471c9387bcdc67d7
Greg Revill <145134799+GregRevFr@users.noreply.github.com>
2023-12-14 10:40:08 +0100

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -298,9 +298,9 @@ def build_photounion_page():
     # Custom CSS to hide the file uploader status
     st.title("PhotoUnion")
     st.write("PhotoUnion groups your similar images together using advanced machine learning techniques.")
-    st.write("PhotoUnion groups your similar images together using advanced machine learning techniques.")
-    st.write("PhotoUnion groups your similar images together using advanced machine learning techniques.")
-    st.write("PhotoUnion groups your similar images together using advanced machine learning techniques.")
+    st.write("--")
+    st.write("--")
+    st.write("--")
 
     uploaded_credentials = st.file_uploader("Upload Google API Credentials", type=["json"])
 
